ed5df9133cf11a2b3e3218c39b26fa8f89da90b0
Amine Yesilyurt <amine.yesilyurt@gmail.com>
2018-01-27 18:01:03 +0300

    sending different contents according different requests, sending, piping a html file to client side

--- http_server.js
+++ http_server.js
@@ -1,23 +1,39 @@
- var http=require('http');
+var http=require('http');
+var filesystem=require('fs');
+function myfun(request,response){
+	//roota erişildiğinde
+	if(request.url=='/'){
+		
+		//console.log(request); //alınan paketin içeriği konsola yazdirildi
 
- function myfun(request,response){
-	 console.log(request); //alınan paketin içeriği yazdirildi
-	 
-	 //repsonse paketi olusturma
-	 response.writeHeader(200,{'Context-Tpe':'text-plain'});
-	 //paketin icerigi
-	 var icerik= '<html><body><h1>';
-	 for(i=1; i<=10;++i)
-		icerik += i+'. Computer Science<br>';
-	 icerik += '</h1></body></html>';
-	 
-	 response.write(icerik);
-	 response.end();
-	 
- }
- http.createServer(myfun).listen(8000);
- console.log('server working');
- /* 
- http paketler seklinde calisir(paket alir,paket gönderir),
- application layerda calisir
- */
+		//repsonse paketi olusturma
+		response.writeHeader(200,{'Context-Tpe':'text-plain'});
+		//paketin icerigi
+		var icerik= '<html><body><h1>';
+		for(i=1; i<=10;++i)
+			icerik += i+'. Computer Science<br>';
+		icerik += '</h1></body></html>';
+
+		response.write(icerik);
+		response.end();
+	}
+	else if(request.url=='/teapot'){
+			response.writeHeader(200,{'Context-Tpe':'text-plain'});
+			filesystem.createReadStream('./teapot.html').pipe(response);
+			//dosyadan okuduklarım respons'a aktarılır
+			
+	}
+	else{
+		response.writeHeader(404,{'Context-Tpe':'text-plain'});	
+		response.write('Sory we don\'t understand where would you go?');
+		response.end();
+	}
+
+ 
+}
+http.createServer(myfun).listen(8000);
+console.log('server working');
+/* 
+http paketler seklinde calisir(paket alir,paket gönderir),
+application layerda calisir
+*/
